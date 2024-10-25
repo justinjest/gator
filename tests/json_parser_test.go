f440ac8f3e470889e73c2cd3418aed1cc7ef159b
justinjest <jest.cayaban@gmail.com>
2024-10-25 11:12:34 +0900

    Build for testing

--- tests/json_parser_test.go
+++ tests/json_parser_test.go
@@ -6,7 +6,7 @@ import (
 
 func TestgetConfigPath(t *testing.T) {
 	want := "~/gatorconfig.json"
-	path, err := json_parser.getConfigFilePath()
+	path, err := getConfigFilePath()
 	if !want.MatchString(path) || err != nil {
 		t.Fatalf(`~/gatorconfig.json = %q, %v, want match for %#q, nil`, path, err, want)
 	}
