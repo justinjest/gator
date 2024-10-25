b3fe8df6c0746b5a648fa545222d1f15961a5afc
justinjest <jest.cayaban@gmail.com>
2024-10-25 10:18:45 +0900

    Working on json reader

--- internal/config/json_parser.go
+++ internal/config/json_parser.go
@@ -1,1 +1,39 @@
 package json_parser
+
+import (
+	"encoding/json"
+	"os"
+)
+
+type Config struct {
+	Db_url            string `json: "db_url"`
+	Current_user_name string `json: "current_user_name"`
+}
+
+const configFileName = `.gatorconfig.json`
+
+func getConfigFilePath() (string, error) {
+	homeDir, err := os.UserHomeDir()
+	if err != nil {
+		return "", err
+	}
+	filePath := homeDir + configFileName
+	return filePath, nil
+}
+
+func Read() (*Config, error) {
+	path, err := getConfigFilePath()
+	if err != nil {
+		return nil, err
+	}
+	data, err := os.ReadFile(path)
+	if err != nil {
+		return nil, err
+	}
+	var config Config
+	if err := json.Unmarshal(data, &config); err != nil {
+		return nil, err
+	}
+
+	return &config, nil
+}
