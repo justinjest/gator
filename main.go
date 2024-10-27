8a9f99dd6d4e476375d71101ed8e05c3dbb0689e
justinjest <jest.cayaban@gmail.com>
2024-10-27 10:48:29 +0900

    Working on adding handler structs to main

--- main.go
+++ main.go
@@ -1,24 +1,60 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 	"log"
 
 	json_parser "github.com/justinjest/gator/internal/config"
 )
 
+type state struct {
+	state *json_parser.Config
+}
+
+type command struct {
+	name string
+	args []string
+}
+
+type commands struct {
+	method map[string]func(*state, command) error
+}
+
+func (c *commands) register(name string, f func(*state, command) error) {
+	c.method[name] = f
+}
+
+func (c *commands) run(s *state, cmd command) error {
+	err := c.method[cmd.name](s, cmd)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func handlerLogin(s *state, cmd command) error {
+	if len(cmd.args) == 0 {
+		return errors.New("no arguments passed to login function")
+	}
+	if len(cmd.args) >= 2 {
+		return errors.New("login takes exactly one argument")
+	}
+	name := cmd.args[0]
+	_, err := json_parser.SetUser(*s.state, name)
+	if err != nil {
+		return err
+	}
+	fmt.Printf("Username set to %v", name)
+	return nil
+}
+
 func main() {
+	var s state
 	config, err := json_parser.Read()
 	if err != nil {
 		log.Fatalf("Error %v", err)
 	}
-	_, err = json_parser.SetUser(config, "Jessica")
-	if err != nil {
-		log.Fatalf("Error %v", err)
-	}
-	newConfig, err := json_parser.Read()
-	if err != nil {
-		log.Fatalf("Error %v", err)
-	}
-	fmt.Printf("%v\n", newConfig)
+	s.state = &config
+
 }
