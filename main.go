daa99de575a6e23e3ea866be2e33d835bd6aecdb
justinjest <jest.cayaban@gmail.com>
2024-10-27 13:09:02 +0900

    Added main function, set up cmdline arguments, setup login

--- main.go
+++ main.go
@@ -3,13 +3,13 @@ package main
 import (
 	"errors"
 	"fmt"
-	"log"
+	"os"
 
 	json_parser "github.com/justinjest/gator/internal/config"
 )
 
 type state struct {
-	state *json_parser.Config
+	config *json_parser.Config
 }
 
 type command struct {
@@ -35,26 +35,46 @@ func (c *commands) run(s *state, cmd command) error {
 
 func handlerLogin(s *state, cmd command) error {
 	if len(cmd.args) == 0 {
-		return errors.New("no arguments passed to login function")
+		return errors.New("user name requried for login function")
 	}
 	if len(cmd.args) >= 2 {
 		return errors.New("login takes exactly one argument")
 	}
 	name := cmd.args[0]
-	_, err := json_parser.SetUser(*s.state, name)
+	_, err := json_parser.SetUser(*s.config, name)
 	if err != nil {
 		return err
 	}
-	fmt.Printf("Username set to %v", name)
+	fmt.Printf("Username set to %v\n", name)
 	return nil
 }
 
 func main() {
-	var s state
 	config, err := json_parser.Read()
 	if err != nil {
-		log.Fatalf("Error %v", err)
+		fmt.Printf("%v\n", err)
+		os.Exit(1)
 	}
-	s.state = &config
-
+	c := commands{
+		method: make(map[string]func(*state, command) error),
+	}
+	c.register("login", handlerLogin)
+	if len(os.Args) < 2 {
+		err = errors.New("too few cmdline arguments")
+	}
+	if err != nil {
+		fmt.Printf("Error %v", err)
+		os.Exit(1)
+	}
+	cmd := command{
+		os.Args[1],
+		os.Args[2:],
+	}
+	var s state
+	s.config = &config
+	err = c.run(&s, cmd)
+	if err != nil {
+		fmt.Printf("Error %v\n", err)
+		os.Exit(1)
+	}
 }
