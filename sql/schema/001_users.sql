377ec23a7be32902637efad929e6ffb1890c119a
justinjest <jest.cayaban@gmail.com>
2024-10-29 09:43:25 +0900

    Created feeds table

--- sql/schema/001_users.sql
+++ sql/schema/001_users.sql
@@ -6,5 +6,14 @@ updated_at timestamp NOT NULL,
 name text UNIQUE NOT NULL
 );
 
+CREATE TABLE feeds(
+    id text PRIMARY KEY,
+    created_at timestamp NOT NULL,
+    updated_at timestamp NOT NULL,
+    name text NOT NULL,
+    url text UNIQUE NOT NULL,
+    user_id text references users(id) ON DELETE CASCADE
+);
+
 -- +goose Down
 DROP TABLE users;
